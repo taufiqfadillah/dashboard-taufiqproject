250b215164fea1b9b4e61eccde0ad3c70641bf8f
fuungt <fuungt@gmail.com>
2023-07-29 18:40:21 +0700

    update

--- config/key.js
+++ config/key.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
-  MongoURI: 'mongodb+srv://taufiqproject:Melangkah01@taufiqproject.vdxxw66.mongodb.net/?retryWrites=true&w=majority',
+  MongoURI: process.env.MongoURI,
 };
