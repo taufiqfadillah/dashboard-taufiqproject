077a9a64b93829999a22a42a069269b4b52f9cd5
fuungt <fuungt@gmail.com>
2023-08-09 19:24:12 +0700

    fixed routes

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated } = require('../config/checkAuth');
 
-//------------ Welcome Route ------------//
+// Welcome Route
 router.get('/', (req, res) => {
   res.render('login');
 });
 
-//------------ Dashboard Route ------------//
+// Dashboard Route
 router.get('/dashboard', ensureAuthenticated, (req, res) =>
   res.render('theme/index', {
     layout: 'theme/layout',
@@ -15,17 +15,75 @@ router.get('/dashboard', ensureAuthenticated, (req, res) =>
   })
 );
 
-//------------ Grouped Theme Routes ------------//
+// Individual Theme Routes
+router.get('/add-post', (req, res) =>
+  res.render('theme/add-post', {
+    layout: 'theme/layout',
+    name: req.user.name,
+  })
+);
 
-const themeFiles = ['add-post', 'blog-single', 'blog', 'chat-video', 'chat', 'edit-profile', 'file-manager', 'index', 'to-do', 'user-profile'];
+router.get('/blog-single', (req, res) =>
+  res.render('theme/blog-single', {
+    layout: 'theme/layout',
+    name: req.user.name,
+  })
+);
 
-for (const file of themeFiles) {
-  router.get(`/${file}`, (req, res) =>
-    res.render(`theme/${file}`, {
-      layout: 'theme/layout',
-      name: req.user.name,
-    })
-  );
-}
+router.get('/blog', (req, res) =>
+  res.render('theme/blog', {
+    layout: 'theme/layout',
+    name: req.user.name,
+  })
+);
+
+router.get('/chat-video', (req, res) =>
+  res.render('theme/chat-video', {
+    layout: 'theme/layout',
+    name: req.user.name,
+  })
+);
+
+router.get('/chat', (req, res) =>
+  res.render('theme/chat', {
+    layout: 'theme/layout',
+    name: req.user.name,
+  })
+);
+
+router.get('/edit-profile', (req, res) =>
+  res.render('theme/edit-profile', {
+    layout: 'theme/layout',
+    name: req.user.name,
+  })
+);
+
+router.get('/file-manager', (req, res) =>
+  res.render('theme/file-manager', {
+    layout: 'theme/layout',
+    name: req.user.name,
+  })
+);
+
+router.get('/index', (req, res) =>
+  res.render('theme/index', {
+    layout: 'theme/layout',
+    name: req.user.name,
+  })
+);
+
+router.get('/to-do', (req, res) =>
+  res.render('theme/to-do', {
+    layout: 'theme/layout',
+    name: req.user.name,
+  })
+);
+
+router.get('/user-profile', (req, res) =>
+  res.render('theme/user-profile', {
+    layout: 'theme/layout',
+    name: req.user.name,
+  })
+);
 
 module.exports = router;
