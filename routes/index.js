172fc0dc9f12168cdc1785acacaaaffaef2cfff8
fuungt <fuungt@gmail.com>
2023-08-09 19:32:35 +0700

    fixed

--- routes/index.js
+++ routes/index.js
@@ -15,8 +15,7 @@ router.get('/dashboard', ensureAuthenticated, (req, res) =>
   })
 );
 
-// Individual Theme Routes
-router.get('/add-post', (req, res) =>
+router.get('/add-post', ensureAuthenticated, (req, res) =>
   res.render('theme/add-post', {
     layout: 'theme/layout',
     name: req.user.name,
@@ -26,63 +25,54 @@ router.get('/add-post', (req, res) =>
 router.get('/blog-single', (req, res) =>
   res.render('theme/blog-single', {
     layout: 'theme/layout',
-    name: req.user.name,
   })
 );
 
 router.get('/blog', (req, res) =>
   res.render('theme/blog', {
     layout: 'theme/layout',
-    name: req.user.name,
   })
 );
 
 router.get('/chat-video', (req, res) =>
   res.render('theme/chat-video', {
     layout: 'theme/layout',
-    name: req.user.name,
   })
 );
 
 router.get('/chat', (req, res) =>
   res.render('theme/chat', {
     layout: 'theme/layout',
-    name: req.user.name,
   })
 );
 
 router.get('/edit-profile', (req, res) =>
   res.render('theme/edit-profile', {
     layout: 'theme/layout',
-    name: req.user.name,
   })
 );
 
 router.get('/file-manager', (req, res) =>
   res.render('theme/file-manager', {
     layout: 'theme/layout',
-    name: req.user.name,
   })
 );
 
 router.get('/index', (req, res) =>
   res.render('theme/index', {
     layout: 'theme/layout',
-    name: req.user.name,
   })
 );
 
 router.get('/to-do', (req, res) =>
   res.render('theme/to-do', {
     layout: 'theme/layout',
-    name: req.user.name,
   })
 );
 
 router.get('/user-profile', (req, res) =>
   res.render('theme/user-profile', {
     layout: 'theme/layout',
-    name: req.user.name,
   })
 );
 
