1c20c6876409c71ea1fd76410c11db626a31b002
fuungt <fuungt@gmail.com>
2023-08-10 01:07:43 +0700

    fix

--- config/passport.js
+++ config/passport.js
@@ -6,24 +6,25 @@ const User = require('../models/User');
 
 module.exports = function (passport) {
   passport.use(
-    new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
-      try {
-        const user = await User.findOne({ email });
-
+    new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+      //------------ User Matching ------------//
+      User.findOne({
+        email: email,
+      }).then((user) => {
         if (!user) {
           return done(null, false, { message: 'This email ID is not registered' });
         }
 
-        const isMatch = await bcrypt.compare(password, user.password);
-
-        if (isMatch) {
-          return done(null, user);
-        } else {
-          return done(null, false, { message: 'Password incorrect! Please try again.' });
-        }
-      } catch (error) {
-        return done(error);
-      }
+        //------------ Password Matching ------------//
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+          if (err) throw err;
+          if (isMatch) {
+            return done(null, user);
+          } else {
+            return done(null, false, { message: 'Password incorrect! Please try again.' });
+          }
+        });
+      });
     })
   );
 
