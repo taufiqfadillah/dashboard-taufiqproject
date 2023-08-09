524353546194def4884c1052026fac60e5e6e087
fuungt <fuungt@gmail.com>
2023-08-09 22:08:23 +0700

    auth fix

--- config/checkAuth.js
+++ config/checkAuth.js
@@ -3,9 +3,13 @@ const User = require('../models/User');
 //------------ Routing via Auth ------------//
 module.exports = {
   ensureAuthenticated: function (req, res, next) {
+    console.log('Session:', req.session);
+    console.log('User:', req.user);
+
     if (req.isAuthenticated()) {
       return next();
     }
+
     req.flash('error_msg', 'Please log in first!');
     res.redirect('/auth/login');
   },
