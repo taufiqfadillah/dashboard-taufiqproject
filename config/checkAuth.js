09998d3ac48cc185e518aa89d81f73ddda80a4a1
fuungt <fuungt@gmail.com>
2023-08-10 11:49:15 +0700

    update

--- config/checkAuth.js
+++ config/checkAuth.js
@@ -11,4 +11,10 @@ module.exports = {
     req.flash('error_msg', 'Please log in first!');
     res.redirect('/auth/login');
   },
+  forwardAuthenticated: function (req, res, next) {
+    if (!req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect('/dashboard');
+  },
 };
