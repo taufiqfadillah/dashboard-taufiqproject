9b5ed985ed7f50a247be01b4d5e0d78711867159
fuungt <fuungt@gmail.com>
2023-08-10 12:34:05 +0700

    UPDATE

--- config/checkAuth.js
+++ config/checkAuth.js
@@ -1,9 +1,32 @@
-const ensureAuthenticated = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  req.flash('error_msg', 'Please log in to view that resource');
-  res.redirect('/users/login');
-};
+const passport = require('passport');
+const User = require('../models/User');
 
-module.exports = { ensureAuthenticated };
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+//------------ Routing via Auth ------------//
+module.exports = {
+  ensureAuthenticated: function (req, res, next) {
+    console.log('Session:', req.session);
+    console.log('User:', req.user);
+
+    if (req.isAuthenticated()) {
+      return next();
+    }
+
+    req.flash('error_msg', 'Please log in first!');
+    res.redirect('/auth/login');
+  },
+  forwardAuthenticated: function (req, res, next) {
+    if (!req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect('/dashboard');
+  },
+};
